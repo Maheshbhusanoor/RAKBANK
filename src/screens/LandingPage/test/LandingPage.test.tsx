d961623e81dc1c33ebcd68acc3713d595b5ab209
mahesh.bhusanoor@alshaya.com <mahesh.bhusanoor@alshaya.com>
2022-12-10 20:09:54 +0400

    Enzyme integrated

--- src/screens/LandingPage/test/LandingPage.test.tsx
+++ src/screens/LandingPage/test/LandingPage.test.tsx
@@ -1,11 +1,48 @@
 import {LandingPage} from '../LandingPage';
-import { render } from 'react-native-testing-library'
+import { render,act} from 'react-native-testing-library'
+import { create } from 'react-test-renderer';
+import { shallow } from "enzyme";
 
-// beforeEach(jest.clearAllMocks)
+beforeEach(jest.clearAllMocks)
+
 describe('Landing Page', () => {
+    const wrapper = shallow(<LandingPage/>);
+
     it('Render Landing Page', async () => {
           const tree = render(<LandingPage />)
 
           expect(tree).toMatchSnapshot()
     });
+    it('Create Landing Page', () => {
+        let root
+        act(() => {
+            root = create(<LandingPage />) 
+        })
+         expect(root).toMatchSnapshot()
+    })
+
+   it('Login Button Props Validation for ', function () {
+        expect(wrapper.find('Button').first().props()).toEqual({
+      onPress: expect.any(Function),
+      background:true,
+      title:"Login with User ID"
+    });
+    });
+ 
+    it('Submit Button Props Validation ', function () {
+        expect(wrapper.find('Button').last().props()).toEqual({
+        onPress: expect.any(Function),
+        background:true,
+        title:"Submit",
+        disabled: true,
+        loading:undefined,
+        top:'4%'
+        });
+    });
+    it('Buttons title to be defined', function () {
+        wrapper.find('Button').forEach( (node) => {
+        expect(node.prop('title')).toBeDefined()
+        })
+    });
+ 
 });
